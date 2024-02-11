a26902401d1fad36731e8e097a4da573805dd580
Samuel Pereira De Almeida <90366093+Samuel2834e@users.noreply.github.com>
2024-02-10 21:09:39 -0300

    Add files via upload

--- js/carregarmsg.js
+++ js/carregarmsg.js
@@ -7,22 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function carregarmsg() {
         const numeroDaSala = document.getElementById('sala').value;
-        const database = getDatabase();
-        const mensagensRef = ref(database, 'Mensagens/' + numeroDaSala);
-        onValue(mensagensRef, (snapshot) => {
-            const data = snapshot.val();
-            const divConteudo = document.getElementById('mensagens');
-            divConteudo.innerHTML = '';
-            if (data) {
-                for (let id in data) {
-                    let p = document.createElement('p');
-                    p.textContent = data[id].nome + ': ' + data[id].mensagem;
-                    divConteudo.appendChild(p);
+            const database = getDatabase();
+            const mensagensRef = ref(database, 'Mensagens/' + numeroDaSala);
+            onValue(mensagensRef, (snapshot) => {
+                const data = snapshot.val();
+                const divConteudo = document.getElementById('mensagens');
+                divConteudo.innerHTML = '';
+                if (data) {
+                    for (let id in data) {
+                        let span = document.createElement('span'); // Use <span> instead of <p>
+                        span.className = "friend last";
+                        span.innerHTML = "<strong>" + data[id].nome + "</strong>" + "   " + data[id].mensagem;
+                        divConteudo.appendChild(span);
+                    }
+                } else {
+                    divConteudo.textContent = 'Nenhuma mensagem encontrada para a sala ' + numeroDaSala;
                 }
-            } else {
-                divConteudo.textContent = 'Nenhuma mensagem encontrada para a sala ' + numeroDaSala;
-            }
-        });
+            });
     }
 
     document.getElementById('showChatBtn').addEventListener('click', carregarmsg);
